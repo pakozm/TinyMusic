4a62dfaf1f0de6f195313b3d667f0d2de5c9cf6d
Paco Zamora Martinez <pakozm@gmail.com>
2016-04-19 22:29:15 +0200

    Updated sketch

--- KeyChainPlayer/TimerFreeTone.cpp
+++ KeyChainPlayer/TimerFreeTone.cpp
@@ -13,13 +13,13 @@ void TimerFreeTone(uint8_t pin, uint8_t pin2, uint32_t frequency, uint32_t durat
   if (!frequency) { delay(duration); return; } // Frequency is false (zero), nothing to play, just delay for duration and return.
   uint32_t notch = 500000 / frequency;       // Calculate how fast to toggle the pin.
   uint32_t loops = (duration * 500) / notch; // Calculate the number of loops to run.
-  pinMode(pin, OUTPUT);                                                                // Set pin to output mode.
+  pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   for (uint32_t i=0; i<loops; i++) { // Loop for the desired number of loops.
-    digitalWrite(pin,HIGH);    // Set pin high.
+    digitalWrite(pin1,HIGH);    // Set pin high.
     digitalWrite(pin2,LOW);    // Set pin low.
     delayMicroseconds(notch);  // Square wave duration (how long to leave pin high).
-    digitalWrite(pin,LOW);     // Set pin low.
+    digitalWrite(pin1,LOW);     // Set pin low.
     digitalWrite(pin2,HIGH);     // Set pin high.
   }
 }
